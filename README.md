154dc08c09d821dfb919ccc748430afad69da72e
Coche <70551980+coche745@users.noreply.github.com>
2021-04-01 13:42:32 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Mysterious Organism
+## Table of Contents
++ General Info
++ Technologies
++ Setup
++ Status
+## General Info
+I created this project as part of Codecademy's curriculum. The scenerio below is made up.
+
+Context: I'm part of a research team that has found a new mysterious organism at the bottom of the ocean near hydrothermal vents. My team names the organism, Pila aequor (P. aequor), and finds that it is only comprised of 15 DNA bases. The small DNA samples and frequency at which it mutates due to the hydrothermal vents make P. aequor an interesting specimen to study. However, P. aequor cannot survive above sea level and locating P. aequor in the deep sea is difficult and expensive. My job is to create objects that simulate the DNA of P. aequor for my research team to study.
+
+To better simulate all the possible DNA of the P. aequor, I have created code that can do the following:
++ Make new P. aequors with random DNA
++ Mutate the P. aequor DNA
++ Compare similarities between different P. aequors' DNA
++ Determine whether the P. aequor is likely to survive
++ Find the complement DNA strand of the P. aequor
++ Find the two most related P. aequors based on DNA out of a lot of different P. aequors.
+
+## Technologies
++ Javascript
+## Setup
+To run this project, install it locally using git
++ git clone https://github.com/coche745/mysterious-organism
+## Status
+Project is: finished
---
+++ main.js
@@ -0,0 +1,143 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+  const dnaBases = ['A', 'T', 'C', 'G'];
+  return dnaBases[Math.floor(Math.random() * 4)];
+};
+
+// Returns a random single strand of DNA containing 15 bases
+const mockUpStrand = () => {
+  const newStrand = [];
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase());
+  }
+  return newStrand;
+};
+
+const pAequorFactory = (num, arr) => {
+  return {
+    specimenNum: num,
+    dna: arr,
+    
+    mutate () {
+      let randIndex = Math.floor(Math.random() * 15);
+      let randBase = this.dna[randIndex];
+      let newRandBase = returnRandBase();
+      while (this.dna[randIndex] === randBase) {
+        if (newRandBase === randBase) {
+          newRandBase = returnRandBase();
+        } else {
+          this.dna[randIndex] = newRandBase;
+        }
+      }
+      return this.dna;
+    },
+  
+    compareDNA (pAequorObj) {
+      let count = 0;
+      let dnaLen = this.dna.length;
+      for (let i = 0; i < dnaLen; i++) {
+        if (this.dna[i] === pAequorObj.dna[i]) {
+          count += 1;
+        }
+      }
+      let percentSimilar = count / dnaLen * 100;
+      console.log(`Specimen # ${this.specimenNum} and specimen # ${pAequorObj.specimenNum} have ${percentSimilar}% DNA in common`);
+      return percentSimilar;
+    },
+
+    willLikelySurvive () {
+      let count = 0;
+      for (let i = 0; i < this.dna.length; i++) {
+        if (this.dna[i] === 'C' || this.dna[i] === 'G') {
+          count += 1;
+        }
+      }
+      let percentCG = count / this.dna.length * 100;
+      return percentCG >= 60;
+    },
+
+    complementStrand () {
+      let newStrand = [];
+      for (let i = 0; i < this.dna.length; i++) {
+        if (this.dna[i] === 'C') {
+          newStrand[i] = 'G';
+        } else if (this.dna[i] === 'G'){
+          newStrand[i] = 'C';
+        } else if (this.dna[i] === 'T') {
+          newStrand[i] = 'A';
+        } else if (this.dna[i] === 'A') {
+          newStrand[i] = 'T'
+        }
+      }
+      return newStrand;
+    }
+  }
+}
+
+const instancesArr = [];
+
+for (i = 0; i < 30; i++) {
+  let newStrand = mockUpStrand();
+  let newPAequor = pAequorFactory(i + 1, newStrand);
+  while (newPAequor.willLikelySurvive() === false) {
+    newStrand = mockUpStrand();
+    newPAequor = pAequorFactory(i + 1, newStrand);
+  }
+  instancesArr.push(newPAequor);
+}
+
+const findMostRelated = (arr) => {
+  let maxPercent = 0;
+  let mostRelatedArr = [];
+  for (i = 0; i < arr.length; i++) {
+    for (j = 0; j < arr.length; j++) {
+      if (i !== j) {
+        let percent = arr[i].compareDNA(arr[j]);
+        if (percent > maxPercent) {
+          maxPercent = percent;
+          mostRelatedArr = [arr[i], arr[j]];
+        }
+      }
+    }
+  }
+  console.log('\n');
+  mostRelatedArr[0].compareDNA(mostRelatedArr[1]);
+  return mostRelatedArr;
+}
+
+let strand1 = mockUpStrand();
+let strand2 = mockUpStrand();
+let strand3 = ['C', 'G', 'C', 'G', 'C', 'G', 'C', 'G', 'C', 'G', 'A', 'C', 'T', 'A', 'T', 'A'];
+let factory1 = pAequorFactory(1, strand1);
+let factory2 = pAequorFactory(2, strand2);
+let factory3 = pAequorFactory(3, strand3);
+
+console.log('\nRandomly generated organism #1: ', factory1);
+
+console.log('\nOrganism #1 mutated: ');
+factory1.mutate();
+console.log(factory1);
+
+console.log('\nRandomly generated organism #2: ', factory2);
+
+console.log('\n');
+factory1.compareDNA(factory2);
+
+console.log('\nOrganism #3, made with DNA likely to survive: ', factory3);
+
+console.log("\nSpecimens are likely to survive if 60% or more of their DNA are C's or G's.");
+console.log('Will specimen 2 likely survive?', factory2.willLikelySurvive());
+console.log('Will specimen 3 likely survive?', factory3.willLikelySurvive());
+
+console.log('\nThe compliment base for G is C and for T is A. Here is Specimen 1 and its compliment DNA: ')
+console.log(factory1);
+console.log(factory1.complementStrand());
+
+/* console.log('\nAn array of 30 randomly generated organisms: ')
+console.log(instancesArr); */
+
+console.log('\nFinding the two organisms from the 30 randomly generated with the most similar DNA:\n')
+console.log(findMostRelated(instancesArr));
+
+
+
